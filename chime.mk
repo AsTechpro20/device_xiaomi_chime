551a39fdf1ac8c978ece19b04f312dce6028617e
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-12-03 19:56:19 +0530

    Update chime.mk

--- chime.mk
+++ chime.mk
@@ -191,6 +191,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
+# Lawnchair
+PRODUCT_PACKAGES += \
+    Lawnchair
+
 PRODUCT_PACKAGES += \
     libcrypto-v33
 
